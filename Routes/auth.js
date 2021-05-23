fcda724dc41f77dd7819f08cf5872b92f280a3e5
kandarp dangi <kkandarpk7@gmail.com>
2021-05-23 20:13:56 +0530

    init

--- Routes/auth.js
+++ Routes/auth.js
@@ -1,18 +1,31 @@
+//Import required packages
 const router = require('express').Router()
 const User = require('../Models/User')
 
+//Validation import
+const Joi = require('joi')
+
+//Defining Joi validation schema
+const schema = Joi.object({
+  name: Joi.string().min(6).required(),
+  email: Joi.string().min(6).required().email().lowercase(),
+  password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+})
+
 router.post('/register', async (req, res) => {
-  const user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-  })
-  try {
-    const savedUser = await user.save()
-    res.send(savedUser)
-  } catch (err) {
-    res.status(400).send(err)
-  }
+  const validation = await schema.validateAsync(req.body)
+  res.send(validation)
+  // const user = new User({
+  //   name: req.body.name,
+  //   email: req.body.email,
+  //   password: req.body.password,
+  // })
+  // try {
+  //   const savedUser = await user.save()
+  //   res.send(savedUser)
+  // } catch (err) {
+  //   res.status(400).send(err)
+  // }
 })
 
 module.exports = router
